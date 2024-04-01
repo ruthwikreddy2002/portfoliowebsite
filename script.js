7bc7271b9d0ae67ebaba26f8935ede7640bbf8bf
ruthwikreddy2002 <165554043+ruthwikreddy2002@users.noreply.github.com>
2024-04-01 17:52:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,64 +4,16 @@
 
 
 
-$(document).ready(function () {
 
-  $('#menu').click(function () {
-      $(this).toggleClass('fa-times');
-      $('.navbar').toggleClass('nav-toggle');
-  });
 
-  $(window).on('scroll load', function () {
-      $('#menu').removeClass('fa-times');
-      $('.navbar').removeClass('nav-toggle');
 
-      if (window.scrollY > 60) {
-          document.querySelector('#scroll-top').classList.add('active');
-      } else {
-          document.querySelector('#scroll-top').classList.remove('active');
-      }
+const form=document.getElementById('myform');
+ 
+function handleClick(event) {
+    // Your other JavaScript code for the div
+    event.preventDefault(); // Prevent default behavior (redirection)
+  }
 
-      // scroll spy
-      $('section').each(function () {
-          let height = $(this).height();
-          let offset = $(this).offset().top - 200;
-          let top = $(window).scrollTop();
-          let id = $(this).attr('id');
-
-          if (top > offset && top < offset + height) {
-              $('.navbar ul li a').removeClass('active');
-              $('.navbar').find(`[href="#${id}"]`).addClass('active');
-          }
-      });
-  });
-
-  // smooth scrolling
-  $('a[href*="#"]').on('click', function (e) {
-      e.preventDefault();
-      $('html, body').animate({
-          scrollTop: $($(this).attr('href')).offset().top,
-      }, 500, 'linear')
-  });
-
-  // <!-- emailjs to mail contact form data -->
-//   $("#contact-form").submit(function (event) {
-//       emailjs.init("O1k-TUb4fIyh-3SK3");
-
-//       emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
-//           .then(function (response) {
-//               console.log('SUCCESS!', response.status, response.text);
-//               document.getElementById("contact-form").reset();
-//               alert("Form Submitted Successfully");
-//           }, function (error) {
-//               console.log('FAILED...', error);
-//               alert("Form Submission Failed! Try Again");
-//           });
-//       event.preventDefault();
-//   });
-  // <!-- emailjs to mail contact form data -->
-  
-
-});
 
 document.addEventListener('visibilitychange',
   function () {
@@ -70,7 +22,7 @@ document.addEventListener('visibilitychange',
           $("#favicon").attr("href", "assets/images/favicon.png");
       }
       else {
-          document.title = "Dude come back ";
+        
           $("#favicon").attr("href", "assets/images/favhand.png");
       }
   });
@@ -84,6 +36,12 @@ var typed = new Typed(".typing-text", {
     backSpeed: 40,
     backDelay: 500,
 });
+form.addEventListener('submit',(event)=>{
+
+    form.reset();
+    
+    });
+  document.title = "Dude come back ";
 // <!-- typed js effect ends -->
 
 
