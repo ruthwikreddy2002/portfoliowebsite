6cc40e90dd7ae0aac0f96895b7f3817bc5946155
ruthwikreddy2002 <165554043+ruthwikreddy2002@users.noreply.github.com>
2024-04-01 13:40:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 200,
+        "value": 100,
         "density": {
           "enable": true,
-          "value_area": 600
+          "value_area": 900
         }
       },
       "color": {
